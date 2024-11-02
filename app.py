15c2a597a1712e4307d034cfe386ff5706d353d5
urimanul <myopera9@gmail.com>
2024-11-03 08:56:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from io import BytesIO
 
 def read_pdf(file):
-    pdf_reader = PdfFileReader(file)
+    pdf_reader = PdfReader(file)
     text = ""
-    for page_num in range(pdf_reader.getNumPages()):
-        page = pdf_reader.getPage(page_num)
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
         text += page.extract_text()
     return text
 
@@ -18,7 +18,7 @@ uploaded_file = st.file_uploader("PDFファイルを選択してください", t
 if uploaded_file is not None:
     text = read_pdf(uploaded_file)
     
-    # 2000行要約をする（sentences_count=2000なので、2000行）
+    # 3行要約をする（sentences_count=3なので、3行）
     parser = PlaintextParser.from_string(text, Tokenizer('japanese'))
     summarizer = LexRankSummarizer()
     res = summarizer(document=parser.document, sentences_count=3)
